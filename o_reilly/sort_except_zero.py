f7e70f63e3feea38224737c8f8191af15b1d9a0c
Shiiq <anjunatown@gmail.com>
2023-03-24 00:06:32 +0300

    Same as last commit with changes

--- o_reilly/sort_except_zero.py
+++ o_reilly/sort_except_zero.py
@@ -10,13 +10,28 @@ from typing import Iterable
 
 
 def except_zero(items: list) -> Iterable:
-    pass
+    zero_positions = []
+    non_zeros = []
+    result = []
+    for pos, num in enumerate(items):
+        if num == 0:
+            zero_positions.append(pos)
+        elif num != 0:
+            non_zeros.append(num)
+    non_zeros.sort()
+    result.extend(non_zeros)
+    for pos in zero_positions:
+        result.insert(pos, 0)
+    return result
 
-DATA = [5, 3, 0, 0, 4, 1, 4, 0, 7]
-var_1 = sorted(DATA)
-var_2 = sorted(DATA, key=lambda x: x==0)
-var_3 = sorted(DATA, key=lambda x: x!=0)
+if __name__ == '__main__':
+    print("Example:")
+    print(list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])))
 
-print(var_1)
-print(var_2)
-print(var_3)
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+    assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+    assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+    assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+    assert list(except_zero([0, 0])) == [0, 0]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
